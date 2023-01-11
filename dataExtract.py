91fb40c5ff77133c0111a560bd2f8a78c2c197b3
SkidroRLD <srathore@ualberta.ca>
2023-01-10 17:07:27 -0700

    Separate Datasets - different implementation

--- dataExtract.py
+++ dataExtract.py
@@ -33,17 +33,27 @@ class TravellingPassenger(Dataset):
     def __getitem__(self, index):
         return torch.asarray(self.data[index]).type(torch.float).to(device), torch.Tensor(self.pid[index]).type(torch.float).to(device)
 
-def read_csv(path = path):
+def read_csv(path = path, modeltype = 'Pytorch'):
     train = pd.read_csv(path / 'train.csv')
     test = pd.read_csv(path / 'test.csv')
+    train.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis = 1)
+    test.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis = 1)
+    embarked_dict = {'S': 0, 'C': 1, 'Q': 2}
+    train['Embarked'] = train['Embarked'].fillna(3)
+    test['Embarked'] = test['Embarked'].fillna(3)
+    train['Embarked'] = train['Embarked'].map(embarked_dict)
+    test['Embarked'] = test['Embarked'].map(embarked_dict)
+    if modeltype == 'Pytorch':
+        train, test = convert_csv(train, test)  
+    return train, test
+
+def convert_csv(train, test):
     train = train.to_numpy()
     test = test.to_numpy()
     train[train == "male"] = 1
     train[train == "female"] = 0
     test[test == "male"] = 1
     test[test == "female"] = 0
-    train = np.delete(train, [3, 8, 10, 11], axis = 1)
-    test = np.delete(test, [2, 7, 9, 10], axis = 1)
     np.random.shuffle(train)
     train = TravelledPassenger(train) #there is a lack of ages, fix that
     test = TravellingPassenger(test)
